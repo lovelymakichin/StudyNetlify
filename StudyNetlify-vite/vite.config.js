fcf8229f4b68bbd2103d1d1212d46d4eb000827b
lovelymakichin <makiss.makiss@gmail.com>
2026-08-06 09:24:54 +0900

    netlifyでPage白になるための対処

--- StudyNetlify-vite/vite.config.js
+++ StudyNetlify-vite/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+export default defineConfig(({ mode }) => {
+	// .envファイルを使うためのもの
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  // modeがproduction(本番環境)ならprocess.env.VITE_PRODUCTION_PATHを設定
+  // modeがproductionでない(開発環境)ならルートパスを設定
+  let basePath = (mode === "production" ? process.env.VITE_PRODUCTION_PATH : "./");
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+  return {
+    base: basePath,
+    plugins: [
+      react(),
+      env({ prefix: "VITE", mountedPath: "process.env" }),
+    ]
+  };
+});
